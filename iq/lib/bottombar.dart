33d4062a4e70fa4eb5f1f514b96771981783a514
Felix <55344513+Phelickz@users.noreply.github.com>
2020-06-13 23:10:03 +0100

    Update bottombar.dart

--- iq/lib/bottombar.dart
+++ iq/lib/bottombar.dart
@@ -6,7 +6,7 @@ import 'package:example/settings.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
-import 'package:flutter/services.dart;
+import 'package:flutter/services.dart';
 
 class MyHomePage extends StatefulWidget {
   @override
